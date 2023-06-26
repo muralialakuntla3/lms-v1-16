0bd33c6ed8eeccc778a49889306eedb856bcfe07
muralialakuntla3 <muralialakuntla3@gmail.com>
2023-06-26 14:28:27 +0530

    dependent softwares

---
+++ install.sh
@@ -0,0 +1,33 @@
+# installation of nginx and unzip
+
+sudo apt install nginx -y
+Sudo apt install unzip
+
+# java installation
+
+sudo apt install openjdk-11* -y
+
+# jenkins installation
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+
+# docker installation
+
+curl -fsSL https://get.docker.com -o install-docker.sh
+sudo sh install-docker.sh
+# Adding permissions
+sudo usermod –aG docker ubuntu
+newgrp docker
+
+# nodejs installation
+
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - 
+sudo apt-get install -y nodejs
+
+
