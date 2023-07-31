eb531ed197ec40c592a8a206f202f6be680d63b4
muralialakuntla3 <muralialakuntla3@gmail>
2023-07-31 03:43:57 -0700

    modifications

--- ansible.tf
+++ ansible.tf
@@ -1,4 +1,4 @@
-# Ansible Node
+# Ansible web Node
 resource "aws_instance" "lmsweb" {
   ami           = "ami-08e5424edfe926b43"
   instance_type = "t2.micro"
@@ -8,13 +8,19 @@ resource "aws_instance" "lmsweb" {
   user_data = file("install_ansible.sh")
   private_ip = "10.0.1.11"
 
+  root_block_device {
+    volume_size           = "10"
+    volume_type           = "gp3"
+    encrypted             = true
+    delete_on_termination = true
+  }
 
   tags = {
-    Name = "Ansible Node"
+    Name = "lms web"
   }
 }
 
-# Ansible Node
+# Ansible api Node
 resource "aws_instance" "lmsapi" {
   ami           = "ami-08e5424edfe926b43"
   instance_type = "t2.micro"
@@ -22,9 +28,14 @@ resource "aws_instance" "lmsapi" {
   key_name = "9-key"
   vpc_security_group_ids = [aws_security_group.lms-sg.id]
   private_ip = "10.0.2.11"
-
+  root_block_device {
+    volume_size           = "10"
+    volume_type           = "gp3"
+    encrypted             = true
+    delete_on_termination = true
+  }
 
   tags = {
-    Name = "Ansible Node"
+    Name = "lms api"
   }
 }
--- install_ansible.sh
+++ install_ansible.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
-git clone -b terraform https://muralialakuntla3-admin@bitbucket.org/muralialakuntla3/lms-app.git /home/ubuntu
-cd lms-app
 ansible-playbook lmsweb.yml -i aws.ini
 ansible-playbook lmsapi.yml -i aws.ini
