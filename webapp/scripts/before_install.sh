067a72af582c6949ab2081f821216f36781a5e8a
muralialakuntla3 <muralialakuntla3@gmail.com>
2023-08-10 21:05:59 +0530

    node installation

--- webapp/scripts/before_install.sh
+++ webapp/scripts/before_install.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 cd /home/ubuntu
 
+#!/bin/bash
+
 # Check if Node.js is already installed
-if command -v node &>/dev/null; then
+if ! command -v node &>/dev/null; then
+    # Update package list
+    sudo apt update
+
+    # Install necessary dependencies
+    sudo apt install -y curl
+
+    # Download and install Node.js 16
+    curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+    sudo apt install -y nodejs
+
+    # Verify Node.js and npm installation
+    node -v
+    npm -v
+
+    echo "Node.js 16 installation completed."
+else
     echo "Node.js is already installed. Skipping installation."
-    exit 0
 fi
 
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
 sudo apt install nginx -y
 sudo systemctl start nginx
