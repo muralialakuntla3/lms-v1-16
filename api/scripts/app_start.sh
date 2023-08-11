dc3bc3ae5ad657e629d461c9b8a0ded899117718
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2023-08-11 01:34:21 -0700

    Update app_start.sh

--- api/scripts/app_start.sh
+++ api/scripts/app_start.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 # Change to your application directory
-sudo npm install pm2 -g
-sudo npx prisma db push
-sudo npm run build
+cd /home/ubuntu/build
+
+# Install PM2 globally
+npm install pm2 -g
+
+# Push Prisma database changes
+npx prisma db push
+
+# Install dependencies and build the application
+npm install
+npm run build
+
 # Start the application with PM2
 NODE_PORT=3000 pm2 start -i 0 build/src/index.js
