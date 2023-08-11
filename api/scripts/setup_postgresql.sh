14bf08ccc91a80df4a074d4eb7bf95359b987801
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2023-08-11 03:06:06 -0700

    Update setup_postgresql.sh

--- api/scripts/setup_postgresql.sh
+++ api/scripts/setup_postgresql.sh
@@ -2,26 +2,41 @@
 
 cd /home/ubuntu
 
-# Add PostgreSQL repository to sources.list
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+# Check if PostgreSQL is already installed
+if ! command -v psql &> /dev/null; then
+    echo "PostgreSQL is not installed. Proceeding with installation."
 
-# Import PostgreSQL repository key
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+    # Add PostgreSQL repository to sources.list
+    sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
-# Update package list
-sudo apt-get update
+    # Import PostgreSQL repository key
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
-# Install PostgreSQL
-sudo apt-get -y install postgresql
+    # Update package list
+    sudo apt-get update
+
+    # Install PostgreSQL
+    sudo apt-get -y install postgresql
+
+    echo "PostgreSQL installation completed."
+else
+    echo "PostgreSQL is already installed. Skipping installation."
+fi
 
 # PostgreSQL username and password
 db_user="postgres"
 db_password="password"
 db_name="lmsdb"
 
-# Create a new PostgreSQL user and database
-sudo -u postgres psql -c "CREATE USER $db_user WITH PASSWORD '$db_password';"
-sudo -u postgres psql -c "CREATE DATABASE $db_name;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $db_name TO $db_user;"
+# Check if the database already exists
+if sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -qw $db_name; then
+    echo "Database '$db_name' already exists. Skipping creation."
+else
+    # Create a new PostgreSQL user and database
+    sudo -u postgres psql -c "CREATE USER $db_user WITH PASSWORD '$db_password';"
+    sudo -u postgres psql -c "CREATE DATABASE $db_name;"
+    sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $db_name TO $db_user;"
 
-echo "PostgreSQL installation and user/database creation completed."
+    echo "PostgreSQL user and database creation completed."
+fi
+
